9fb193d8645d24f52b692a380927189fbd0f0f35
阿部直也 <n.abe@gemcook.com>
2019-05-29 20:57:16 +0900

    [update] resolve

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,5 +3,10 @@ export const CHANGE = 'CHANGE';
 
 export const onSubmitClick = text => ({
   type: SUBMIT,
-  text: text,
+  text,
+});
+
+export const handleChange = value => ({
+  type: CHANGE,
+  value,
 });
