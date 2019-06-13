31032970df4f5c1a93eb1d3231259ab9a0614a01
阿部直也 <n.abe@gemcook.com>
2019-06-13 10:03:02 +0900

    [update] application削除

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: '************',
-  authDomain: '************',
-  databaseURL: '************',
-  projectId: '************',
-  storageBucket: '************',
-  messagingSenderId: '************',
-  appId: '************',
+  apiKey: 'AIzaSyDy-7ZTBYY_p31dqe-LOA96tbMQ8vsIIEs',
+  authDomain: 'pigtalk-1fb2e.firebaseapp.com',
+  databaseURL: 'https://pigtalk-1fb2e.firebaseio.com',
+  projectId: 'pigtalk-1fb2e',
+  storageBucket: 'pigtalk-1fb2e.appspot.com',
+  messagingSenderId: '1077798988983',
+  appId: '1:1077798988983:web:e8dbcc5a903c349d',
 };
